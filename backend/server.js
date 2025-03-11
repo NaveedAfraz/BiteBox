fb6b60d276d96bc5abc3fc856ebbb701903652d9
NaveedAfraz <naveedafraz123@gmail.com>
2025-03-12 03:30:07 +0530

    latest stuff

--- backend/server.js
+++ backend/server.js
@@ -3,18 +3,21 @@ const app = express();
 const dotenv = require("dotenv");
 const path = require("path");
 const cors = require("cors");
+const cloudinary = require("./cloudinary"); 
 app.use(
   cors({
     origin: "http://localhost:5173",
-    method: [POST, GET, PUT, DELETE],
+    method: ["POST", "GET", "PUT", "DELETE"],
   })
 );
+app.use(express.json({ limit: "10mb" })); 
+app.use(express.urlencoded({ extended: true }));
 
 dotenv.config();
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
-
+ 
 const port = 3006;
 app.listen(port, () => {
   console.log(`Server is running on port http://localhost:${port}`);
